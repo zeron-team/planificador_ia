9a4405ec75ff26d52c6852a6937456f0dd74352c
zeron_educar31 <renzoantonioli@outlook.es>
2024-08-31 18:14:46 +0000

    version 1.5

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+# Cargar las variables de entorno desde un archivo .env
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '7899784eaec0c6f9933159debce762dd1ce621741b0858ba'
-    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY') or 'REDACTED'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
